721ad70e643571d9c7dd2476c5e3e7b8f190c43f
Joe Quattrone <joequattrone5@gmail.com>
2018-10-16 14:33:45 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+def countdown(n)
+  phrase = "HAPPY NEW YEAR!"
+  counter = 0
+  
+  while counter < n do
+    puts "#{n} SECCOND(s)!"
+    counter += 1 
+  end
 end
